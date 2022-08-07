771ff96737ae1f846bb32a6f15764dc5467500c5
Parmeshwar Badad <107673796+parmeshwarbadad@users.noreply.github.com>
2022-08-07 20:47:58 +0530

    SQLQuery2.sql

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+create table emp1
+(emp_id int primary key,
+frist_name varchar(20) not null,
+last_name varchar(20) not null,
+);
+
+insert into emp1 values(2,'amol','ghuge');
+insert into emp1 values(3,'pralhad','shinde');
+insert into emp1 values(4,'anuj','jagtap');
